721a860ee449d57414fe8b552174008afa252082
Ionlycomedheretotestlol <carloseduardonplus1@gmail.com>
2026-08-29 18:02:21 -0300

    Fix buildFeatures formatting, targetSdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,20 @@ android {
     defaultConfig {
         applicationId = "com.spywareonmyphon"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0-NOMERCY"
     }
     buildFeatures {
-        aidl = true compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.10" }
-    kotlinOptions { jvmTarget = "17" }
+        aidl = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
